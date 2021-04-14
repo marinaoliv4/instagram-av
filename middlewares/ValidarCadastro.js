995ea6e419645d31a784bf96a39e563673e697e5
Marina Oliveira <marinaoliv4@gmail.com>
2021-04-14 14:52:35 -0300

    middleware no cadastro

---
+++ middlewares/ValidarCadastro.js
@@ -0,0 +1,29 @@
+const {Usuario} = require('../models')
+
+module.exports = async (request, response, next) => {
+    const {nome, email, senha} = request.body; //recebo o email q o usuario digitou
+
+    if (nome == null || email == null || senha == null) {
+    response.status(400).json({ erro: 'preencha os campos obrigatórios'});
+    }
+ 
+    let user = await Usuario.findAll({where: {email}}); //verifica se existe usuario c o email
+    
+    if(user.length) { 
+        response.status(400).json({erro:"email já cadastrado"})
+        return;
+    }else{
+        if(!email){
+            response.status(400).json({erro:"por gentileza informe o email ae"})
+        }else{
+            if (senha.length < 6 || senha.length > 12) {
+                response.status(400).json({erro: "sua senha deve conter entre 6 e 12 digitos"});
+            } else {
+                next();    
+            }
+            
+            
+        }
+    }
+}
+
--- routes/usuariosRouter.js
+++ routes/usuariosRouter.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const usuariosController = require('../controllers/usuariosController');
 const router = express.Router();
+const validarCadastro = require('../middlewares/ValidarCadastro')
 
 /* GET users listing. */
 router.get('/', usuariosController.index);
-
-router.post('/', usuariosController.create);
-
+//validarCadastro está como middleware
+router.post('/', validarCadastro, usuariosController.create);
 //especificar por id
 router.put('/:id', usuariosController.update);
-
 router.delete('/:id', usuariosController.delete);
 
 
